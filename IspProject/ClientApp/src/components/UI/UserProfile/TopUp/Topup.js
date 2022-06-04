17b9f105988a56e9bec505c0442f08b18dbcd9e8
vldktsn <vlad.K2001@gmail.com>
2022-06-04 22:02:16 +0300

    Air siren and missile danger lol

--- IspProject/ClientApp/src/components/UI/UserProfile/TopUp/Topup.js
+++ IspProject/ClientApp/src/components/UI/UserProfile/TopUp/Topup.js
@@ -3,9 +3,26 @@ import React from "react";
 import classes from "./Topup.module.css";
 
 const Topup = () => {
-    return(
-        <div>Topup Balance</div>
-    );
-}
+  return (
+    <div className={classes.general}>
+      <div className={classes.wrapper}>
+        <h1>Top Up Balance</h1>
+        <form>
+          <div className={classes.form__input__group}>
+            <b>Amount</b>
+            <input
+              type="number"
+              name="Amount"
+              id="amount"
+              placeholder="Enter amount"
+              pattern="[0-9]*"
+            />
+            <button>Pay</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Topup;
